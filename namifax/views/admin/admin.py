a1989adddd0d880f6d08be7ce7b02f4ff70e29e3
ji-ho lee <search5@mz.co.kr>
2022-04-21 16:22:44 +0900

    기본 로그인 기능 구현 중(손대지 말자고 불과 하루도 안지난 것 같은데...)

--- namifax/views/admin/admin.py
+++ namifax/views/admin/admin.py
@@ -8,52 +8,7 @@ from cerberus import Validator
 
 @view_config(route_name='admin_login')
 def admin_login(request):
-
-
-    """ 
-    /******************************************************************************************************************************
-    SETUP FORM RULES
-    ******************************************************************************************************************************/
-    $formdata = new FormRules;
-    $formdata->newRule('username');
-    $formdata->newRule('password');
-    $formdata->newRule('_submit_check');
-
-    $_SESSION[USERSESSION] = new AFUserAccount;
-
-    /******************************************************************************************************************************
-            PROCESS FORM
-     ******************************************************************************************************************************/
-    if (array_key_exists('_submit_check', $_POST)) {
-        $formdata->processForm($_POST);
-
-        if ($formerror = $formdata->getFormErrors()) {
-            $error = "<li>".join("<li>", $formerror);
-        }
-
-        if (!$error) {
-            if ($ALTERNATE_AUTH_ENABLE) {
-                if ($_SESSION[USERSESSION]->login_alternate_auth($formdata->username, $formdata->password, true)) {
-                    header("Location: admin.php");
-                    exit;
-                }
-            }
-
-            if (!$ALTERNATE_AUTH_ENABLE || ($ALTERNATE_AUTH_ENABLE && $ALTERNATE_AUTH_FALLBACK)) {
-                if ($_SESSION[USERSESSION]->login($formdata->username, $formdata->password, true)) {
-                    if ($_SESSION[USERSESSION]->is_expired()) {
-                        header("Location: pwdexpired.php");
-                    } else {
-                        header("Location: admin.php");
-                    }
-                    exit;
-                }
-            }
-
-            $error = $_SESSION[USERSESSION]->get_error();
-        }
-    }
-    """
+    return {}
 
 
 @view_config(route_name='admin_index', renderer='namifax:templates/admin/index.jinja2')
@@ -67,13 +22,33 @@ def admin_index(request):
 
         if is_valid:
             return HTTPFound(location=request.route_path('index'))
+        else:
+            errors = StringIO()
+            errors.write('<ul>')
+            for key, val in v.errors.items():
+                errors.write(f'<li>{key}: {"<br>".join(val)}</li>')
+            errors.write('</ul>')
 
-        errors = StringIO()
-        errors.write('<ul>')
-        for key, val in v.errors.items():
-            errors.write(f'<li>{key}: {"<br>".join(val)}</li>')
-        errors.write('</ul>')
+            # if ($ALTERNATE_AUTH_ENABLE) {
+            #     if ($_SESSION[USERSESSION]->login_alternate_auth($formdata->username, $formdata->password, true)) {
+            #         header("Location: admin.php");
+            #         exit;
+            #     }
+            # }
+            #
+            # if (!$ALTERNATE_AUTH_ENABLE || ($ALTERNATE_AUTH_ENABLE && $ALTERNATE_AUTH_FALLBACK)) {
+            #     if ($_SESSION[USERSESSION]->login($formdata->username, $formdata->password, true)) {
+            #         if ($_SESSION[USERSESSION]->is_expired()) {
+            #             header("Location: pwdexpired.php");
+            #         } else {
+            #             header("Location: admin.php");
+            #         }
+            #         exit;
+            #     }
+            # }
+            #
+            # $error = $_SESSION[USERSESSION]->get_error();
 
-        return dict(error=Markup(errors.getvalue()))
+            return dict(error=Markup(errors.getvalue()))
 
     return {}
